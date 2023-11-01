ee5c2a7ef253eb5cd8b6aeb7915afb74c9768375
Louis Cheng <ycheng33@academic.rrc.ca>
2023-11-01 16:31:18 -0500

    Populating database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,47 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "csv"
+
+Pokemon.delete_all
+Trainer.delete_all
+Region.delete_all
+
+ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name='pokemons';")
+ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name='trainers';")
+ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name='regions';")
+
+file = Rails.root.join("db/pokemon.csv")
+puts "Loading Pokemon: #{file}"
+
+CSV.foreach(file, headers: true) do |row|
+    pokemon_data = {
+      name: row["name"],
+      attack: row["attack"],
+      hp: row["hp"],
+      defense: row["defense"],
+      sp_attack: row["sp_attack"],
+      sp_defense: row["sp_defense"],
+      speed: row["speed"],
+      type_one: row["type1"],
+      type_two: row["type2"]
+    }
+  
+    Pokemon.create(pokemon_data)
+  end
+
+  50.times do
+    Trainer.create(
+      name: Faker::Name.name,
+      gender: Faker::Gender.binary_type,
+      age: Faker::Number.between(from: 16, to: 65)
+    )
+  end
+  
+used_locations = []
+
+25.times do
+  unique_location = nil
+  while unique_location.nil? || used_locations.include?(unique_location)
+    unique_location = Faker::Games::Pokemon.location
+  end
+  Region.create(location: unique_location)
+  used_locations << unique_location
+end
