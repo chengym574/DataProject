a94549cc64325aa97a247ed2c6f37fcf6e136828
Louis Cheng <ycheng33@academic.rrc.ca>
2023-11-02 16:43:02 -0500

    Added routes to show pages

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :pokemons, only: [:index]
+  resources :pokemons, only: [:index, :show]
 
-  resources :trainers, only: [:index]
+  resources :trainers, only: [:index, :show]
 
-  resources :regions, only: [:index]
+  resources :regions, only: [:index, :show]
 end
